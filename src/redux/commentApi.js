db8b508e2d53e02b39385525e5fb53fa9fedd00b
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-05-16 17:36:00 +0300

    1

--- src/redux/commentApi.js
+++ src/redux/commentApi.js
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const API_ENDPOINT = '/comments';
 const BASE_URL = '';
 
+
 export const commentApi = createApi({
   reducerPath: 'comments',
   baseQuery: fetchBaseQuery({
